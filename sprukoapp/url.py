e112cdc3d8be7d223c58173331b39978d06b8b55
sanfeer-en <sanfi7254@gmail.com>
2023-05-08 14:44:12 +0530

    edit commit

--- sprukoapp/url.py
+++ sprukoapp/url.py
@@ -1,7 +1,10 @@
 from django.urls import path 
-from .views import  * 
+from .views import * 
 urlpatterns = [
     path("home" ,index , name="dashboard") , 
-    path('forms',formpage , name="formclick"),
-    path('tabel',table , name="tabel")  
+    path('forms',form , name="formclick"),
+    path('table',table , name="tabel"),
+    path("delete/<int:id>", delete , name="delete"), 
+    path("edit/<int:id>", edit , name="edit"),
+    
 ]
--- sprukoapp/views.py
+++ sprukoapp/views.py
@@ -16,10 +16,30 @@ def form(request):
         email = request.POST.get('email')
         phonenumber = request.POST.get('number')
         Dofbirth = request.POST.get('dateofbirth')
-        userdetails = UserDeatils(username = fullname , passwords = password , email = email , number = phonenumber , birth = Dofbirth) 
+        userdetails = UserDeatils.objects.create(username = fullname , passwords = password , email = email , number = phonenumber , birth = Dofbirth) 
         userdetails.save()
+
         return redirect("/table")
     return render(request,'form.html')
 def table(request):
     userinput = UserDeatils.objects.all()
-    return render(request,'table.html',{"userdata": userinput})
+    return render(request,'table.html',{"userdata":userinput})
+def delete(request , id):
+    userinput = UserDeatils.objects.get(id=id)
+    userinput.delete()
+    return redirect("/table")
+
+def edit(request):
+    userinput = UserDeatils.objects.get(id=id)
+    return render(request,'edit.html',{"userdata":userinput})
+def update(request,id):
+    userinput = UserDeatils.objects.get(id=id)
+    if request.method == 'POST' :
+        userinput.userName = request.POST['username']
+        userinput.email = request.POST['email']
+        userinput.phoneNumber = request.POST['number']
+        userinput.password = request.POST['password']
+        userinput.dofbirth = request.POST['dateofbirth']
+        userinput.save()
+        return redirect ("/table")
+    
